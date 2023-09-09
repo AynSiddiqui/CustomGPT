bdf507a3f1d8895c9cba4bbab220dc5350572950
Ayaan <ayaansiddiqui68@gmail.com>
2023-09-09 15:03:20 +0530

    Update Chat.tsx
    Added type a prompt feature when no messages exist or new chat page

--- GPT/components/Chat.tsx
+++ GPT/components/Chat.tsx
@@ -4,7 +4,8 @@ import { collection, orderBy, query } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import Message from "./Message";
-
+import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
+import { useRef } from "react";
 type Props = {
     id: string;
 }
@@ -17,13 +18,23 @@ function Chat({ id }: Props) {
             orderBy('createdAt', 'asc')
         ));
     return (
-        <div className="flex-1 overflow-x-hidden overflow-y-auto">
-            {messages?.docs.map((message) => (
-                <Message key={message.id}
-                    message={message.data()}
-                />)
-            )}
-        </div>
+        <>
+            {messages?.docs.length === 0
+                && (
+                    <div className="flex flex-col  text-slate-300 justify-center items-center">
+                        <h1 className="pt-10 pb-4 text-2xl font-bold opacity-90"> Type a Prompt to begin</h1>
+                        <ArrowDownCircleIcon className=" h-9 w-9 animate-bounce" />
+
+                    </div>
+                )}
+            <div className="flex-1 overflow-x-hidden overflow-y-auto">
+                {messages?.docs.map((message) => (
+                    <Message key={message.id}
+                        message={message.data()}
+                    />)
+                )}
+            </div>
+        </>
     )
 }
 
