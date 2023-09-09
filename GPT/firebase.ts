313e0ec187c02d732a3d48dc716c0928d5368592
Ayaan <103879986+AynSiddiqui@users.noreply.github.com>
2023-09-09 22:37:31 +0530

    Update firebase.ts

--- GPT/firebase.ts
+++ GPT/firebase.ts
@@ -1,15 +1,7 @@
 import {getApp,getApps,initializeApp} from 'firebase/app';
 import {getFirestore} from 'firebase/firestore';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCqDVKEsiy1GL0ezmCpVT1hU1tzPyvg0jw",
-    authDomain: "mygpt-d493f.firebaseapp.com",
-    projectId: "mygpt-d493f",
-    storageBucket: "mygpt-d493f.appspot.com",
-    messagingSenderId: "1059781265856",
-    appId: "1:1059781265856:web:28b0b2588d23f4cdaae025"
-  };
-  
+const firebaseConfig = <ADD YOUR FIREBASE CONFIG>
 // Initialize Firebase
 const app= getApps().length ? getApp() : initializeApp(firebaseConfig);
 const db = getFirestore(app);
